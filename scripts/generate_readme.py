91ee3b7177ea82b305d1df807408b1ee9e18f0f4
Sargis Heiser <sargis.heiser@gmail.com>
2026-01-10 23:36:36 +0100

    Update generate_readme.py
    Initial commit

--- scripts/generate_readme.py
+++ scripts/generate_readme.py
@@ -193,8 +193,9 @@ I’m **Sargis**, an **AI & Full-Stack Engineer** based in **{LOCATION}**.
 
 ## Stats
 
-![Stats](https://github-readme-stats.vercel.app/api?username={USERNAME}&show_icons=true&theme=tokyonight)
-![Streak](https://streak-stats.demolab.com?user={USERNAME}&theme=tokyonight)
+![Stats](https://github-readme-stats.vercel.app/api?username=sargisheiser&show_icons=true&theme=tokyonight)
+![Streak](https://streak-stats.demolab.com?user=sargisheiser&theme=tokyonight)
+
 
 ---
 
